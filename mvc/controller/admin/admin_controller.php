9e215a339582bd109888550627d2deb49c2c8093
HUY <huy20050416@gmail.com>
2026-09-21 19:39:15 +0700

    xoá api

--- mvc/controller/admin/admin_controller.php
+++ mvc/controller/admin/admin_controller.php
@@ -95,7 +95,7 @@ switch ($admin) {
                 header("Refresh: 1.5; url='?act=admin&admin=categoryList'");
             }
         }
-        include 'category/update.php';
+        include 'view/admin/category/update.php';
         // header("location: ?act=admin&admin=categoryList");
         break;
     case 'productList':
@@ -359,7 +359,7 @@ switch ($admin) {
                 $thongBaoBN = "Thêm thành công";
             }
         }
-        include "product/update.php";
+        include "view/admin/product/update.php";
         break;
         $thongBao = "";
         $thongBaoLoi = "";
