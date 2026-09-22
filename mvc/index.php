28692357db8c3fa2ceaad22752bba2c503389e8b
LaiHuy05 <huy20050416@gmail.com>
2026-09-22 09:23:10 +0700

    Refactor PHP MVC folder structure

--- mvc/index.php
+++ mvc/index.php
@@ -1,16 +1,16 @@
 <?php
 // mvc/index.php
 session_start(); 
-include_once './query/pdo.php';
+include_once './config/database.php';
 
 $act = $_GET['act'] ?? 'client';
 
 if ($act === 'logout') {
     // Xử lý logout thẳng ở đây hoặc gọi file logout
-    include_once 'view/client/login/logout.php';
+    include_once 'views/client/login/logout.php';
 } elseif ($act === 'admin' || isset($_GET['admin'])) {
-    include_once 'controller/admin/admin_controller.php';
+    include_once 'controllers/admin/admin_controller.php';
 } else {
     // Luồng client
-    include_once 'controller/client/client_controller.php';
+    include_once 'controllers/client/client_controller.php';
 }
